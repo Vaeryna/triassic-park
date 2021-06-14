cc74f65df02026ecea224de4ea4f12f6431c1cb1
unknown <you@example.com>
2021-06-14 15:12:49 +0200

    panier lié au compte

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Panier, Total } from '../data/panier';
 import { ProduitService } from '../produit.service';
 import { Produit } from '../data/produit';
-import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
-import { elementAt } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-panier',
@@ -21,35 +20,13 @@ export class PanierComponent implements OnInit {
 
   ngOnInit(): void {
     if (this.mail)
-      /*  this.pS.getClientId(this.mail).subscribe((id) => {
-          this.clientID = id; */
       this.pS.getPanierClient(this.mail).subscribe((panier) => {
-        console.log('paneir', panier), (this.panier = Object.values(panier));
-        console.log('undefinefd', this.panier);
         this.panier.forEach((element) => {
-          console.log('element 0', element);
           this.pS.getProduitPrice(element.name).subscribe(() => {
             (this.price = element.prix_HT * element.quantite),
               (this.totalPrice = this.totalPrice + this.price);
-
           });
         });
       });
   }
 }
-
-/*     if (this.mail)
-      this.pS.getPanierClient(this.mail).subscribe((a) => {
-        this.panier = a;
-        this.panier.forEach((element) => {
-          this.pS.getProduitPrice(element.name).subscribe(() => {
-            (this.price = element.prix_HT * element.quantite),
-              (this.totalPrice = this.totalPrice + this.price);
-          });
-          this.pS.addPanierPrice(this.totalPrice).subscribe();
-        });
-      });
- */
-//  this.pS.getTotalPricePanier().subscribe((a) => (this.totalPrice = a));
-
-//e: questions$.do(questions => {questions.forEach(q => question here)})
